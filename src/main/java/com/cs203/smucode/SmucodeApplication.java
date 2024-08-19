19e202dd33f87f7f423763012df3d75dbbf7ed42
derej_gnow <jeredwong711@gmail.com>
2024-08-19 17:24:53 +0800

    chore: add scaffolding

---
+++ src/main/java/com/cs203/smucode/SmucodeApplication.java
@@ -0,0 +1,13 @@
+package com.cs203.smucode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmucodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmucodeApplication.class, args);
+	}
+
+}
