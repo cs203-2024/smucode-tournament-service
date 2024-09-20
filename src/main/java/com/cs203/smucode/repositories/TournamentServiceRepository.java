77972697caaf9985389637afd6e8efc48cac81a3
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-09-20 15:38:08 +0800

    Add find tourney by deadline & status

--- src/main/java/com/cs203/smucode/repositories/TournamentServiceRepository.java
+++ src/main/java/com/cs203/smucode/repositories/TournamentServiceRepository.java
@@ -4,6 +4,11 @@ import com.cs203.smucode.models.Tournament;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TournamentServiceRepository extends MongoRepository<Tournament, String> {
+    List<Tournament> findBySignUpDeadlineBeforeAndStatus(LocalDateTime dateTime, String status);
+
 }
