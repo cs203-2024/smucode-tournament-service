37140d2435ccad17c99382e00739c140ff55c1e2
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-09-20 15:15:26 +0800

    Add userDTO definition

--- src/main/java/com/cs203/smucode/dto/UserDTO.java
+++ src/main/java/com/cs203/smucode/dto/UserDTO.java
@@ -1,4 +1,15 @@
 package com.cs203.smucode.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String profileImageUrl;
+    private String role;
+    private double mu;
+    private double sigma;
+    private double skillIndex;
 }
