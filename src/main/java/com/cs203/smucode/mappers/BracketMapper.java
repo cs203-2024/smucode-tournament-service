f9a17a39c2dbd7b81fa9484e73eb9a3f07bb54c3
jered wong <jeredwong711@gmail.com>
2024-10-02 01:13:31 +0800

    feat: add bracketScoreDTO

--- src/main/java/com/cs203/smucode/mappers/BracketMapper.java
+++ src/main/java/com/cs203/smucode/mappers/BracketMapper.java
@@ -1,6 +1,7 @@
 package com.cs203.smucode.mappers;
 
 import com.cs203.smucode.dto.BracketDTO;
+import com.cs203.smucode.dto.BracketScoreDTO;
 import com.cs203.smucode.models.Bracket;
 import org.mapstruct.Mapper;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface BracketMapper {
 
+//    default bracket DTO
     BracketDTO bracketToBracketDTO(Bracket bracket);
 
     List<BracketDTO> bracketsToBracketDTOs(List<Bracket> brackets);
@@ -16,5 +18,16 @@ public interface BracketMapper {
     Bracket bracketDTOToBracket(BracketDTO bracketDTO);
 
     List<Bracket> bracketDTOsToBrackets(List<BracketDTO> bracketDTOs);
+
+//    update bracket score DTO
+    BracketScoreDTO bracketScoreToBracketScoreDTO(Bracket bracket);
+
+    List<BracketScoreDTO> bracketScoresToBracketScoreDTOs(List<Bracket> brackets);
+
+    Bracket bracketScoreDTOToBracketScore(BracketScoreDTO bracketDTO);
+
+    List<Bracket> bracketScoreDTOsToBracketScores(List<BracketScoreDTO> bracketDTOs);
+
+
 
 }
