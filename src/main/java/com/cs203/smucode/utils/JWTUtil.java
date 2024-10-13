1de9c53835eae474c091be1faa51fea030b86376
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-13 19:05:57 +0800

    Fix cors + allow all traffic

--- src/main/java/com/cs203/smucode/utils/JWTUtil.java
+++ src/main/java/com/cs203/smucode/utils/JWTUtil.java
@@ -1,40 +1,22 @@
-//package com.cs203.smucode.utils;
-//
-//import com.nimbusds.jose.JWSVerifier;
-//import com.nimbusds.jose.crypto.RSASSAVerifier;
-//import com.nimbusds.jwt.SignedJWT;
-//import jakarta.annotation.PostConstruct;
-//import java.security.KeyFactory;
-//import java.security.NoSuchAlgorithmException;
-//import java.security.interfaces.RSAPublicKey;
-//import java.security.spec.InvalidKeySpecException;
-//import java.security.spec.X509EncodedKeySpec;
-//import java.util.Base64;
-//import java.util.Date;
-//import java.util.Map;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class JWTUtil {
-//
-//    private JwtDecoder jwtDecoder;
-//
-//    @PostConstruct
-//    public void init(@Value("${jwt.public.key}") String publicKeyString)
-//            throws InvalidKeySpecException, NoSuchAlgorithmException {
-//        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyString);
-//        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-//        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(
-//            new X509EncodedKeySpec(publicKeyBytes)
-//        );
-//        jwtDecoder = NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
-//    }
-//
-//    public Map<String, Object> getClaimsFromToken(String token) {
-//        return jwtDecoder.decode(token).getClaims();
-//    }
-//}
+package com.cs203.smucode.utils;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTUtil {
+
+    private JwtDecoder jwtDecoder;
+
+    @Autowired
+    public JWTUtil(JwtDecoder jwtDecoder) {
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    public Map<String, Object> getClaimsFromToken(String token) {
+        return jwtDecoder.decode(token).getClaims();
+    }
+}
