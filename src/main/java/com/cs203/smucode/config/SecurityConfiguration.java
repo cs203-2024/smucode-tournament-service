cc5091d2c1da6233072dc43368ec5ed73ffb4af3
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-13 19:09:27 +0800

    Allow credentials in cors

--- src/main/java/com/cs203/smucode/config/SecurityConfiguration.java
+++ src/main/java/com/cs203/smucode/config/SecurityConfiguration.java
@@ -61,6 +61,7 @@ public class SecurityConfiguration {
         configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source =
             new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
