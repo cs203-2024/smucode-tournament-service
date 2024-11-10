69f19f15a964064e1cb4bb40a51ec8612de944fd
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-11-10 13:13:43 +0800

    Update TestSecurityConfig.java

--- src/test/java/com/cs203/smucode/configs/TestSecurityConfig.java
+++ src/test/java/com/cs203/smucode/configs/TestSecurityConfig.java
@@ -5,19 +5,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.Instant;
+import java.util.Map;
 
 @TestConfiguration
 public class TestSecurityConfig {
-
+    
     @Bean
-    @Primary
-    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .oauth2ResourceServer(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return token ->
+                new Jwt(
+                        "token",
+                        Instant.now(),
+                        Instant.now().plusSeconds(300),
+                        Map.of("alg", "none"),
+                        Map.of("scope", "read")
+                );
     }
 }
