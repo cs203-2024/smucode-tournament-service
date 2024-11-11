bc7de8363fccbde0badbec2175e34202f1bf2bd9
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-11-11 10:06:35 +0800

    Update TestSecurityConfig.java

--- src/test/java/com/cs203/smucode/configs/TestSecurityConfig.java
+++ src/test/java/com/cs203/smucode/configs/TestSecurityConfig.java
@@ -3,6 +3,7 @@ package com.cs203.smucode.configs;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,30 +11,31 @@ import org.springframework.security.web.SecurityFilterChain;
 import java.time.Instant;
 import java.util.Map;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @TestConfiguration
+@EnableWebSecurity
 public class TestSecurityConfig {
-    
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(STATELESS)
+                );
 
         return http.build();
     }
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return token ->
-                new Jwt(
-                        "token",
-                        Instant.now(),
-                        Instant.now().plusSeconds(300),
-                        Map.of("alg", "none"),
-                        Map.of("scope", "read")
-                );
+        return token -> {
+            return new Jwt(token, Instant.now(), Instant.now().plusSeconds(300),
+                    Map.of("alg", "RS256"), Map.of("sub", "testuser", "scope", "ROLE_USER"));
+        };
     }
 }
